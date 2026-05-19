afdcd50cf8e747eade876e1de6f7e72619613227
AdeleM-prog <12043@holbertonstudents.com>
2026-05-19 14:48:43 +0200

    implement task4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -10,3 +10,39 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacher {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacherFunction: printTeacher = (firstName, lastName) => `${firstName[0]}. ${lastName}`;
+
+
+
+
+interface StudentClassConstructor{
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
